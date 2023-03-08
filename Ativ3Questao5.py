f513e328d5bc9993f08ea4fde2956c036296e565
GiseleBCSantos <125358138+GiseleBCSantos@users.noreply.github.com>
2023-03-08 15:00:31 -0300

    Update Ativ3Questao5.py

--- Ativ3Questao5.py
+++ Ativ3Questao5.py
@@ -1,3 +1,4 @@
-anos = int(input())
-bonus_ano = float(input())
-print(f"{anos*bonus_ano:.2f}")
+anos = int(input("Insira a quantidade de anos que você trabalha na empresa: "))
+bonus_ano = float(input("Insira o valor do bônus por ano trabalhado: "))
+print(f"O funcionário terá a seguinte bonificação: R${anos*bonus_ano:.2f}")
+
