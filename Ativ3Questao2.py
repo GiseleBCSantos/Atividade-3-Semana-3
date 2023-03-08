c3821deb1f574da573ff77264e02fbae05a60d5c
GiseleBCSantos <125358138+GiseleBCSantos@users.noreply.github.com>
2023-03-08 14:55:59 -0300

    Update Ativ3Questao2.py

--- Ativ3Questao2.py
+++ Ativ3Questao2.py
@@ -1,3 +1,4 @@
-preco = float(input())
+preco = float(input("Insira o preço do produto: R$"))
 preco_desc = preco * 0.9
-print(f"{preco_desc:.2f}")
+print(f"O seu produto custa R${preco:.2f} reais e, com desconto de 10%, vai passar a custar: R${preco_desc:.2f} reais.")
+
